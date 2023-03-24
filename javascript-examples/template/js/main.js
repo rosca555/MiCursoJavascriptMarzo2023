936470cb995909135b9c658b53e012f38fdaf728
Your Name <you@example.com>
2023-03-24 13:38:22 +0100

    cambios viernes

--- javascript-examples/template/js/main.js
+++ javascript-examples/template/js/main.js
@@ -1,8 +1,7 @@
-
 // set the dimensions and margins of the graph
-const margin = {top: 20, right: 30, bottom: 30, left: 60},
+const margin = {top: 10, right: 20, bottom: 30, left: 50},
     width = 500 - margin.left - margin.right,
-    height = 600 - margin.top - margin.bottom;
+    height = 420 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 const svg = d3.select("#d3-container")
@@ -10,44 +9,41 @@ const svg = d3.select("#d3-container")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
-    .attr("transform",
-          `translate(${margin.left},${margin.top})`);
+    .attr("transform", `translate(${margin.left},${margin.top})`);
 
-// get the data
-d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/1_OneNum.csv").then( function(data) {
+//Read the data
+d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/4_ThreeNum.csv").then( function(data) {
 
-  // X axis: scale and draw:
+  // Add X axis
   const x = d3.scaleLinear()
-      .domain([0, 1000])     // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
-      .range([0, width]);
+    .domain([0, 10000])
+    .range([ 0, width ]);
   svg.append("g")
-      .attr("transform", `translate(0, ${height})`)
-      .call(d3.axisBottom(x));
+    .attr("transform", `translate(0, ${height})`)
+    .call(d3.axisBottom(x));
 
-  // set the parameters for the histogram
-  const histogram = d3.histogram()
-      .value(function(d) { return d.price; })   // I need to give the vector of value
-      .domain(x.domain())  // then the domain of the graphic
-      .thresholds(x.ticks(70)); // then the numbers of bins
-
-  // And apply this function to data to get the bins
-  const bins = histogram(data);
-
-  // Y axis: scale and draw:
+  // Add Y axis
   const y = d3.scaleLinear()
-      .range([height, 0]);
-      y.domain([0, d3.max(bins, function(d) { return d.length; })]);   // d3.hist has to be called before the Y axis obviously
+    .domain([50, 90])
+    .range([ height, 0]);
   svg.append("g")
-      .call(d3.axisLeft(y));
+    .call(d3.axisLeft(y));
 
-  // append the bar rectangles to the svg element
-  svg.selectAll("rect")
-      .data(bins)
-      .join("rect")
-        .attr("x", 1)
-    .attr("transform", function(d) { return `translate(${x(d.x0)} , ${y(d.length)})`})
-        .attr("width", function(d) { return x(d.x1) - x(d.x0) -1})
-        .attr("height", function(d) { return height - y(d.length); })
-        .style("fill", "#69b3a2")
+  // Add a scale for bubble size
+  const z = d3.scaleLinear()
+    .domain([200000, 1310000000])
+    .range([ 1, 40]);
 
-});
+  // Add dots
+  svg.append('g')
+    .selectAll("dot")
+    .data(data)
+    .join("circle")
+      .attr("cx", d => x(d.gdpPercap))
+      .attr("cy", d => y(d.lifeExp))
+      .attr("r", d => z(d.pop))
+      .style("fill", "blue")
+      .style("opacity", "0.9")
+      .attr("stroke", "red")
+
+})
